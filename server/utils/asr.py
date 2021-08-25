8cc0064edeb29c5ed624c9629596cfeebeb5c650
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2021-08-25 14:31:28 +0530

    added standard telugu asr to the server utils

---
+++ server/utils/asr.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+
+
+def telugu(audio_url) -> str:
+	print(f'Performing ASR on audio: {audio_url}')
+	url = 'http://canvas.iiit.ac.in/iiitASR/runASR'
+	headers = {
+		'Content-Type': 'application/json',
+	}
+	payload = {
+		'audioURL': audio_url
+	}
+
+	r = requests.post(url, headers=headers, data=json.dumps(payload))
+	ret = r.json()['scriptedText'].strip()
+	print(f'Telugu ASR output: {ret}')
+	return ret
